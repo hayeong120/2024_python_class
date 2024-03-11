cc56e5047f1a2e5b7035f05ddfe8128c694712c6
hayeong120 <jbg01200249@gmail.com>
2024-03-11 11:15:45 +0900

    3월 11일 수업코드

---
+++ pythonProject/Test.py
@@ -0,0 +1,13 @@
+import turtle as t
+t.shape("turtle")
+t.forward(100)
+t.left(144)
+t.forward(100)
+t.left(144)
+t.forward(100)
+t.left(144)
+t.forward(100)
+t.left(144)
+t.forward(100)
+t.left(144)
+t.exitonclick()
